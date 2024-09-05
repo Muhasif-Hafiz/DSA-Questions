8eef3aa1b4523272e0789fbf76e45ea46a104938
Muhasif-Hafiz <muhasibhafeez91@gmail.com>
2024-09-05 23:57:33 +0530

    MaxSum

---
+++ src/MaxSum.java
@@ -0,0 +1,10 @@
+public class MaxSum {
+    public static void main(String[] args) {
+        int [] arr={1,23,65,56,250};
+        int ans=0;
+        for(int i=0;i< arr.length;i++){
+            ans+=arr[i];
+        }
+        System.out.println(ans);
+    }
+}
